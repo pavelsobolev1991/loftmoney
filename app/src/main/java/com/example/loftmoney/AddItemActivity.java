c7b6f81a9116eb7a97ce40edd8b4a3babba60d78
Pavel <pavelsobolev1991@gmail.com>
2020-03-22 11:49:40 +0300

    AddItemActivity

---
+++ app/src/main/java/com/example/loftmoney/AddItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItemAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item_acitivity);
+
+        
+    }
+}
